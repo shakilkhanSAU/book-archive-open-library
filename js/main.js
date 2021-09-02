a3f1f23b5fa38fcd184f2eed47f5362ef157fc03
shakilkhanSAU <shakilkhan.sau@gmail.com>
2021-09-02 21:30:38 +0600

    ui design improved

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 const bookContainer = document.getElementById('book-area');
 const searchResult = document.getElementById('result');
+const backgroundImage = document.getElementById('background');
 // error section display none 
 document.getElementById('error-message').style.display = 'none';
 
 const loadData = () => {
-    //spiner show 
+    //spiner, result conatiner, background and result counter funtioning
     document.getElementById('spinner').classList.remove('d-none');
-    // book conatiner cleaning
     bookContainer.textContent = '';
-    // search result counter cleaning
     searchResult.textContent = '';
+    backgroundImage.classList.add('d-none');
     // hide error message
     document.getElementById('error-message').style.display = 'none';
     // fetching data from api
@@ -19,21 +19,23 @@ const loadData = () => {
     searchField.value = '';
     fetch(url)
         .then(res => res.json())
-        .then(data => displayData(data.docs))
+        .then(data => displayData(data))
 };
 
-const displayData = data => {
+const displayData = dataObject => {
+    const data = dataObject.docs;
+    // when no data found 
     if (data.length === 0) {
         // hide spinner
         document.getElementById('spinner').classList.add('d-none');
         // show error 
         document.getElementById('error-message').style.display = 'block';
+        // search result counter
+        searchResult.innerText = `${dataObject.numFound}`;
     }
     else {
         data.forEach(book => {
-            if (book.author_name !== undefined) {
-                const authorName = book.author_name;
-                const publisher = book?.publisher;
+            if (book.author_name !== undefined && book.publisher !== undefined) {
                 const bookCover = book.cover_i;
                 const bookCoverUrl = `https://covers.openlibrary.org/b/id/${bookCover}-M.jpg`;
                 // hide spinner
@@ -46,20 +48,17 @@ const displayData = data => {
                     <div class="card">
                         <img class="my-image card-img-top img-fluid " src="${bookCoverUrl}" alt="Not Found" onerror=this.src="./image/alternative.jpg">
                         <div class="card-body">
-                            <h5 class="card-title">${book.title}</h5>
-                            <p class="card-text">Author name- <span class="fw-bold">${authorName[0]} </span></p>
-                            <p class="card-text">Publisher- <span class="fw-bold">${publisher}</span></p>
+                            <h4 class="card-title">${book.title}</h4>
+                            <p class="card-text">Author name- <span class="fw-bold">${book.author_name[0]} </span></p>
+                            <p class="card-text">Publisher- <span class="fw-bold">${book.publisher[0]}</span></p>
+                            <p class="card-text">First publish in ${book.first_publish_year}</span></p>
                             
-                        </div>
-                        <div class="card-footer">
-                            <small class="text-muted">First publish in ${book.first_publish_year}</small>
                         </div>
                     </div>
                 `;
                 bookContainer.appendChild(singleBook);
-
                 // search result counter
-                searchResult.innerText = `${data.length}`;
+                searchResult.innerText = `${dataObject.numFound}`;
             };
         });
     }
